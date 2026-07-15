2017c988763c9bdfc87ba271df6c57b7577120fb
vkm10077 <vkm10077@gmail.com>
2026-07-15 18:36:20 +0530

    Refactor config.py to update secret key handling

--- config.py
+++ config.py
@@ -2,37 +2,11 @@ import os
 
 
 class Config:
+    FYERS_CLIENT_ID = os.getenv("FYERS_CLIENT_ID")
+    FYERS_SECRET_KEY = os.getenv("FYERS_SECRET_KEY")
+    FYERS_REDIRECT_URI = os.getenv("FYERS_REDIRECT_URI")
+
     SECRET_KEY = os.getenv(
-        "SECRET_KEY",
-        "change-this-secret-key"
-    )
-
-    FYERS_CLIENT_ID = os.getenv(
-        "FYERS_CLIENT_ID",
-        ""
-    )
-
-    FYERS_SECRET_KEY = os.getenv(
-        "FYERS_SECRET_KEY",
-        ""
-    )
-
-    FYERS_REDIRECT_URI = os.getenv(
-        "FYERS_REDIRECT_URI",
-        ""
-    )
-
-    WHATSAPP_TOKEN = os.getenv(
-        "WHATSAPP_TOKEN",
-        ""
-    )
-
-    WHATSAPP_PHONE_NUMBER_ID = os.getenv(
-        "WHATSAPP_PHONE_NUMBER_ID",
-        ""
-    )
-
-    WHATSAPP_RECIPIENT_NUMBER = os.getenv(
-        "WHATSAPP_RECIPIENT_NUMBER",
-        ""
+        "FLASK_SECRET_KEY",
+        "temporary-development-secret-key"
     )
